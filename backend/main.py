921a6193468d9a6cbf9346952da4e5b1528a2e8c
Arhyeldee001 <divinestephen7319@gmail.com>
2025-07-05 16:00:45 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -232,3 +232,7 @@ def dashboard(request: Request):
 def template_gallery(request: Request):
     return templates.TemplateResponse("templates.html", {"request": request})
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+    
