db79b17a23915565ecda87a3b2e7c9ea54d166a6
Arhyeldee001 <divinestephen7319@gmail.com>
2025-09-06 11:22:38 -0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,22 +1,4 @@
-const CACHE_NAME = 'archi-trace-v1';
-const urlsToCache = [
-  '/',
-  '/static/js/main.js',
-  '/static/css/styles.css',
-  '/static/onboarding/',
-  '/ar'
-];
-
+// Empty service worker - just enough to trigger PWA
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  self.skipWaiting();
 });
